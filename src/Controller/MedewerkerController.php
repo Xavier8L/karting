535021f1d764ace080a45eb5c9bff21497531808
Xavier8L <302864748@student.rocmondriaan.nl>
2022-04-08 12:44:02 +0200

    soort activiteten add verwijden verandern function maak A3 afmaken

--- src/Controller/MedewerkerController.php
+++ src/Controller/MedewerkerController.php
@@ -4,7 +4,9 @@ namespace App\Controller;
 
 
 use App\Entity\Activiteit;
+use App\Entity\Soortactiviteit;
 use App\Form\ActiviteitType;
+use App\Form\SoortactiviteitType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Form\Extension\Core\Type\ResetType;
@@ -22,9 +24,13 @@ class MedewerkerController extends AbstractController
         $activiteiten=$this->getDoctrine()
             ->getRepository('App:Activiteit')
             ->findAll();
+        
+        $soortActiviteiten = $this->getDoctrine()
+            ->getRepository('App:SoortActiviteit')
+            ->findAll();
 
         return $this->render('medewerker/activiteiten.html.twig', [
-            'activiteiten'=>$activiteiten
+            'activiteiten'=>$activiteiten,'soortActiviteiten'=>$soortActiviteiten
         ]);
     }
 
@@ -154,4 +160,109 @@ class MedewerkerController extends AbstractController
         return $this->redirectToRoute('beheer');
 
     }
+
+
+    /**
+     * @Route("/admin/soortAction",name="soortAction")
+     */
+    public function soortActioben()
+    {
+        $soortActiviteiten=$this->getDoctrine()
+            ->getRepository('App:Soortactiviteit')
+            ->findAll();
+
+        return $this->render('medewerker/soort.html.twig', [
+            'activiteiten'=>$soortActiviteiten
+        ]);
+    }
+
+    /**
+     * @Route("/admin/soortAction/add", name="soortActionAdd")
+     */
+    public function addSoortAction(Request $request)
+    {
+        // create a user and a contact
+        $soort=new Soortactiviteit;
+
+        $form = $this->createForm(SoortactiviteitType::class, $soort);
+        $form->add('save', SubmitType::class, array('label'=>"voeg toe"));
+        //$form->add('reset', ResetType::class, array('label'=>"reset"));
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($soort);
+            $em->flush();
+
+            $this->addFlash(
+                'notice',
+                'soort activiteit toegevoegd!'
+            );
+            return $this->redirectToRoute('soortAction');
+        }
+        $activiteiten=$this->getDoctrine()
+            ->getRepository('App:Soortactiviteit')
+            ->findAll();
+        return $this->render('medewerker/soortAdd.html.twig',array('form'=>$form->createView(),'naam'=>'toevoegen','aantal'=>count($activiteiten)
+            ));
+    }
+
+     /**
+     * @Route("/admin/soortAction/update/{id}", name="soortActionUpdate")
+     */
+    public function updateSoortAction($id,Request $request)
+    {
+        $a=$this->getDoctrine()
+            ->getRepository('App:Soortactiviteit')
+            ->find($id);
+
+        $form = $this->createForm(SoortactiviteitType::class, $a);
+        $form->add('save', SubmitType::class, array('label'=>"aanpassen"));
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $em = $this->getDoctrine()->getManager();
+
+            // tells Doctrine you want to (eventually) save the contact (no queries yet)
+            $em->persist($a);
+
+
+            // actually executes the queries (i.e. the INSERT query)
+            $em->flush();
+            $this->addFlash(
+                'notice',
+                'activiteit aangepast!'
+            );
+            return $this->redirectToRoute('soortAction');
+        }
+
+        $activiteiten=$this->getDoctrine()
+            ->getRepository('App:Soortactiviteit')
+            ->findAll();
+
+        return $this->render('medewerker/soortAdd.html.twig',array('form'=>$form->createView(),'naam'=>'aanpassen','aantal'=>count($activiteiten)));
+    }
+
+    /**
+     * @Route("/admin/soortAction/delete/{id}", name="soortActionDelete")
+     */
+    public function deleteSoortAction($id)
+    {
+        $em=$this->getDoctrine()->getManager();
+        $a= $this->getDoctrine()
+            ->getRepository('App:Soortactiviteit')->find($id);
+        $em->remove($a);
+        $em->flush();
+
+        $this->addFlash(
+            'notice',
+            'activiteit verwijderd!'
+        );
+        return $this->redirectToRoute('soortAction');
+
+    }
+
 }
